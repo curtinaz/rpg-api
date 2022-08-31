58d18acd11bd80d5ed75b7a106619775287e2b47
Cristian Curtinaz <curtinazif@gmail.com>
2022-08-31 18:48:56 -0300

    User model fix

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,10 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
 class User extends Model
 {
     use HasFactory;
+    use HasApiTokens;
+    use Notifiable;
+
 
     protected $fillable = [
         "name",
