914e7e4a0df54f012f62fbe467a669f99d6afddb
Cristian Curtinaz <curtinazif@gmail.com>
2022-08-31 15:18:41 -0300

    Users migrations controller and model

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -320,8 +320,8 @@ class ComposerStaticInita7afbac03dfada1633cd6bb8c6a10f0e
         ),
         'Psr\\Http\\Message\\' => 
         array (
-            0 => __DIR__ . '/..' . '/psr/http-message/src',
-            1 => __DIR__ . '/..' . '/psr/http-factory/src',
+            0 => __DIR__ . '/..' . '/psr/http-factory/src',
+            1 => __DIR__ . '/..' . '/psr/http-message/src',
         ),
         'Psr\\Http\\Client\\' => 
         array (
