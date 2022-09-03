5904fc6d8d53a30d0b3d01119e88858acd2e8812
Cristian Curtinaz <curtinazif@gmail.com>
2022-09-02 21:27:15 -0300

    Get Rooms documentation

--- app/Http/Controllers/RoomsController.php
+++ app/Http/Controllers/RoomsController.php
@@ -7,7 +7,20 @@ use Illuminate\Http\Request;
 
 class RoomsController extends Controller
 {
-    public function index() {
+
+    /**
+     * @OA\get(
+     *      path="/api/rooms",
+     *      summary="Retrieve all rooms",
+     *      tags={"Rooms"},
+     *      @OA\Response(
+     *          response=200,
+     *          description="Success",
+     *      )
+     * ),
+     */
+    public function index()
+    {
         return Room::all();
     }
 }
