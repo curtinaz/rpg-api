6698667d6d4db82bf1f6ddd343a3152d09606c78
Cristian Curtinaz <curtinazif@gmail.com>
2022-09-02 21:23:16 -0300

    Get Rooms Route added

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\RoomsController;
 use App\Http\Controllers\UsersController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -7,3 +8,7 @@ use Illuminate\Support\Facades\Route;
 // Users
 Route::post('users/login', [UsersController::class, 'login']);
 Route::post('users/register', [UsersController::class, 'register']);
+
+
+// Rooms
+Route::get('rooms', [RoomsController::class, 'index']);
